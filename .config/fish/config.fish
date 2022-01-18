4499e3f32117c621a033d9bd6eb058029ed940c9
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2022-01-18 10:10:32 +0100

    Adapt for me from Hessel fork

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,12 @@
 #!/usr/bin/env fish
 set fish_greeting
 
+fish_vi_key_bindings
+
 set -gx EDITOR nvim
 set -gx LANG en_US.UTF-8
 
-
-set -gx PATH ~/bin /usr/local/bin /usr/bin /usr/local/sbin
+set -gx PATH ~/.bin /usr/local/bin /usr/bin /usr/local/sbin
 set -gx PATH $PATH usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
 
 set -gx PIP_REQUIRE_VIRTUALENV true
