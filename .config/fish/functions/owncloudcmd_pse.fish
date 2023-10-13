4a70fedb55972806906e9177b3a00d0c57f8ee90
Johannes Sappl <johannes.sappl@chello.at>
2023-10-13 11:09:57 +0200

    refactor: PSE OC mkdir when not exist

--- .config/fish/functions/owncloudcmd_pse.fish
+++ .config/fish/functions/owncloudcmd_pse.fish
@@ -1,3 +1,4 @@
 function owncloudcmd_pse --wraps owncloudcmd --description 'alias Plansee OwnCloud'
+    mkdir -p /data/$argv[1]
     owncloudcmd --user sapplj --password=$(pass show oc.plansee-group.com/login 2>&1 | head -n 1) /data/$argv[1] https://oc.plansee-group.com $argv[1]
 end
