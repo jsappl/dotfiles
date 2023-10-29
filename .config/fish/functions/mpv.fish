acb9423b36af5710e4cc050e7ba0c837293e6bb3
Johannes Sappl <johannes.sappl@chello.at>
2023-10-29 22:27:46 +0100

    feat: mpv alias to limit video resolution

---
+++ .config/fish/functions/mpv.fish
@@ -0,0 +1,4 @@
+function mpv --description 'alias mpv=mpv --ytdl-format="(bestvideo[height<=1080]+bestaudio)[ext=webm]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/bestvideo+bestaudio/best"'
+ command mpv --ytdl-format="(bestvideo[height<=1080]+bestaudio)[ext=webm]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/bestvideo+bestaudio/best" $argv
+        
+end
