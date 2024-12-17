93e12eb90371d8c313bc4b1fbc9d88a9325a2aaa
Johannes Sappl <johannes.sappl@chello.at>
2024-12-17 08:53:06 +0100

    refactor: rm pdm support in favor of uv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,13 +11,7 @@ set -gx PATH ~/.bin /usr/local/bin /usr/bin /usr/local/sbin ~/.local/bin
 set -gx PATH $PATH usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
 
 set -gx PIP_REQUIRE_VIRTUALENV true
-set -gx PYENV_ROOT $HOME/.pyenv
-set -gx PATH $PYENV_ROOT/bin $PATH
 
 set -gx TEXMFHOME ~/.texmf
 
 set -gx PASSWORD_STORE_GENERATED_LENGTH 32
-
-pyenv init --path | source
-pyenv init - | source
-pyenv virtualenv-init - | source
